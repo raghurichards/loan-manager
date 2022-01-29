c60a1f53da555c9a43969d0e13e58b9adec2a197
raghurichards <raghurichards@gmail.com>
2022-01-29 12:50:37 +0400

    initial commit

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://localhost:3000/',
-  // BASE_URL: 'https://loanmanager-d860b.web.app:3000/',
+  // BASE_URL: 'https://loan-manager-heroku.herokuapp.com/',
 
   CUSTOMER_BASE_URL: 'http://localhost:3000/customers/',
   //CUSTOMER_BASE_URL: 'https://loanmanager-d860b.web.app:3000/customers/',
